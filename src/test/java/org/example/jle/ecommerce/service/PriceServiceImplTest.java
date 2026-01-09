1e2bd9fff9b4631edd708ddc5d36a9802e985770
joseluisespana <joseluis.espana@rindus.de>
2026-01-09 09:43:53 +0100

    Updating double type in API in order to remove warning

--- src/test/java/org/example/jle/ecommerce/service/PriceServiceImplTest.java
+++ src/test/java/org/example/jle/ecommerce/service/PriceServiceImplTest.java
@@ -15,7 +15,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
